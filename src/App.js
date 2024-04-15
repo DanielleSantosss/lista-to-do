a9a094ac767a9e38c71879b6d30e3e64c2451716
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-04-15 17:15:09 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs';
+
+const API = 'http://localhost:5000';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [time, setTime] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='todoHeader'>
+        <h1>Lista To Do</h1>
+      </div>
+      <div className='formTodo'>
+        <h2>Insira sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='formControl'>
+            <label htmlFor='title'>O que você precisa fazer?</label>
+            <input
+              type='text'
+              name='title'
+              placeholder='Título da tarefa'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ''}
+              required
+            />
+          </div>
+          <input type='submit' value='Enviar' />
+        </form>
+      </div>
+      <div className='listTodo'>
+        <h2>Lista de tarefas: </h2>
+        {toDos.length === 0 && <p>Não há tarefas pendentes.</p>}
+      </div>
     </div>
   );
 }
