80b7b342952d0063a03d639fea1503a8ba5b674b
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-04-29 19:04:15 -0300

    finalizando a apliação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs';
 
 const API = 'http://localhost:5000';
@@ -10,7 +10,24 @@ function App() {
   const [toDos, setToDos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const response = await fetch(API + '/todos')
+        .then((response) => response.json())
+        .then((data) => (data))
+        .catch((error) => console.log(error))
+
+      setLoading(false)
+      setToDos(response)
+
+      
+    }
+    loadData();
+  }, [])
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const toDo = {
@@ -18,13 +35,52 @@ function App() {
       title,
       time,
       done: false
+    };
+
+    await fetch( API + '/todos', {
+      method: "POST",
+      body: JSON.stringify(toDo),
+      headers:{
+        'Content-Type': 'application/json',
+      },
     }
+    )
 
-    console.log(toDo);
+      setToDos((prevState) => [...prevState, toDo]);
 
-      setTitle('')
-      setTime('')
+      setTitle('');
+      setTime('');
   };
+
+  const handleDelete = async(id) => {
+    await fetch(API + '/todos/' + id, {
+      method: 'DELETE'
+    });
+
+    setToDos((prevState) => prevState.filter((toDo) => toDo.id !== id));
+  };
+
+
+  const data = async(toDo) => {
+
+    toDo.done = !toDo.done;
+
+    await fetch(API + '/todos/' + toDo.id, {
+      method: 'PUT',
+      body: JSON.stringify(toDo),
+      headers:{
+        'Content-Type': 'application/json',
+      }
+    });
+
+    setToDos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+  );
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div className='App'>
@@ -62,6 +118,18 @@ function App() {
       <div className='listToDo'>
         <h2>Lista de tarefas: </h2>
         {toDos.length === 0 && <p>Não há tarefas pendentes.</p>}
+        {toDos.map((toDo) => (
+          <div className='toDo' key={toDo.id}>
+            <h3 className={toDo.done ? 'toDo-done': ''}>{toDo.title}</h3>
+            <p>Duração: {toDo.time}</p>
+            <div className='actions'>
+              <span onClick={() => data(toDo)}>
+                {!toDo.done ? <BsBookmarkCheck/> : <BsBookmarkCheckFill/>}
+              </span>
+              <BsTrash onClick={() => handleDelete(toDo.id)}/>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
